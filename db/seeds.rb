e1862960949b994621947048040324f68e6705eb
brookton <davidbrookton@gmail.com>
2019-05-16 14:15:20 -0600

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+one = Post.create(title: "Hello World", description: "Hows it going")
